86e8b53de0f593e8dbf5d7d211dd06c7090ae401
Yong Liu <yong.liu@ozstrategy.com>
2018-06-01 11:30:05 +0800

    add a space bar

--- CrawlNews.py
+++ CrawlNews.py
@@ -250,7 +250,7 @@ def create_markdown_content(news):
     #     article_content += tpl_article_content.format(title=c['title'], url=c['url'], description=c['description'])
 
     # article = article_title + article_top_summary+'\n' + article_content
-    article = article_top_summary+'\n' + article_content
+    article = article_top_summary+'\n<p>&nbsp;</p>' + article_content
     return title, article
 
 def create_markdown_file(title, article):
